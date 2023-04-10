a275e9eee63210597dab2a86608f10e46beac58a
Dylan Honeyman <dylan.honeyman@encapture.com>
2023-04-09 21:41:37 -0600

    bio
    bio update

--- src/components/Lander.jsx
+++ src/components/Lander.jsx
@@ -10,6 +10,9 @@ export const Lander = () => (
       </StyledH1>
       <StyledH2>
           I'm a full stack developer living and playing in Fort Collins, Colorado. I currently work at Encapture, helping to build a platform to allow customers to automate their document processing solutions.
+          <br />
+          <br />
+          As a recent Colorado transplant I spend most of my free time climbing in Poudre Canyon or tearin up the many gravel bike trails Ft Collins has to offer.
       </StyledH2>
     </Bio>
 
